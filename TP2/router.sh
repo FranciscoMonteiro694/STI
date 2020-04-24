d2e0c7871f26984a96fc88daf1f119b13a091ee4
RicardoJMPaiva <43242841+RicardoJMPaiva@users.noreply.github.com>
2020-04-24 16:00:09 +0100

    Update router.sh

--- TP2/router.sh
+++ TP2/router.sh
@@ -41,8 +41,63 @@ iptables -A FORWARD -s 87.248.214.1 -p tcp -d 23.214.219.133 --dport domain -i e
 iptables -A FORWARD -s 23.214.219.133 -p tcp -d 87.248.214.1 --sport domain -i enp0s9 -o enp0s10 -j ACCEPT
 
 #SMTP connections
+# internal
 iptables -A FORWARD -s 192.168.10.0/24 -p tcp -d 23.214.219.132 --dport smtp -i enp0s8 -o enp0s9 -j ACCEPT
 iptables -A FORWARD -s 23.214.219.132 -p tcp -d 192.168.10.0/24 --sport smtp -i enp0s9 -o enp0s8 -j ACCEPT
+
+# internet
+iptables -A FORWARD -s 87.248.214.0/24 -p tcp -d 23.214.219.132 --dport smtp -i enp0s10 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.132 -p tcp -d 87.248.214.0/24 --sport smtp -i enp0s9 -o enp0s10 -j ACCEPT
+
+
+#IMAP e POP
+
+# Internal network
+iptables -A FORWARD -s 192.168.10.0/24 -p tcp -d 23.214.219.134 --dport pop3 -i enp0s8 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 192.168.10.0/24 -p tcp -d 23.214.219.134 --dport imap -i enp0s8 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.134 -p tcp -d 192.168.10.0/24 --sport pop3 -i enp0s9 -o enp0s8 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.134 -p tcp -d 192.168.10.0/24 --sport imap -i enp0s9 -o enp0s8 -j ACCEPT
+
+#internet
+iptables -A FORWARD -s 87.248.214.0/24 -p tcp -d 23.214.219.134 --dport pop3 -i enp0s10 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 87.248.214.0/24 -p tcp -d 23.214.219.134 --dport imap -i enp0s10 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.134 -p tcp -d 87.248.214.0/24 --sport pop3 -i enp0s9 -o enp0s10 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.134 -p tcp -d 87.248.214.0/24 --sport imap -i enp0s9 -o enp0s10 -j ACCEPT
+
+#HTTP e HTTPS
+#Internal network
+iptables -A FORWARD -s 192.168.10.0/24 -p tcp -d 23.214.219.131 --dport http -i enp0s8 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 192.168.10.0/24 -p tcp -d 23.214.219.131 --dport https -i enp0s8 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.131 -p tcp -d 192.168.10.0/24 --sport http -i enp0s9 -o enp0s8 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.131 -p tcp -d 192.168.10.0/24 --sport https -i enp0s9 -o enp0s8 -j ACCEPT
+
+#Internet
+iptables -A FORWARD -s 87.248.214.0/24 -p tcp -d 23.214.219.131 --dport http -i enp0s10 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 87.248.214.0/24 -p tcp -d 23.214.219.131 --dport https -i enp0s10 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.131 -p tcp -d 87.248.214.0/24 --sport http -i enp0s9 -o enp0s10 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.131 -p tcp -d 87.248.214.0/24 --sport https -i enp0s9 -o enp0s10 -j ACCEPT
+
+#OPENVPN
+
+# Internal network (UDP)
+iptables -A FORWARD -s 192.168.10.0/24 -p udp -d 23.214.219.130 --dport openvpn -i enp0s8 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.130 -p udp -d 192.168.10.0/24 --sport openvpn -i enp0s9 -o enp0s8 -j ACCEPT
+
+# Internal network (TCP)
+iptables -A FORWARD -s 192.168.10.0/24 -p tcp -d 23.214.219.130 --dport openvpn -i enp0s8 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.130 -p tcp -d 192.168.10.0/24 --sport openvpn -i enp0s9 -o enp0s8 -j ACCEPT
+
+#Internet (UDP)
+iptables -A FORWARD -s 87.248.214.0/24 -p udp -d 23.214.219.130 --dport openvpn -i enp0s10 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.130 -p udp -d 87.248.214.0/24 --sport openvpn -i enp0s9 -o enp0s10 -j ACCEPT
+
+#Internet (UDP)
+iptables -A FORWARD -s 87.248.214.0/24 -p tcp -d 23.214.219.130 --dport openvpn -i enp0s10 -o enp0s9 -j ACCEPT
+iptables -A FORWARD -s 23.214.219.130 -p tcp -d 87.248.214.0/24 --sport openvpn -i enp0s9 -o enp0s10 -j ACCEPT
+
+# OPENVPN DATASTORE
+iptables -A FORWARD -s 23.214.219.130 -p tcp -d 192.168.10.3 --dport postgres -i enp0s9 -o enp0s8 -j ACCEPT
+iptables -A FORWARD -s 192.168.10.3 -p tcp -d 23.214.219.130 --sport postgres -i enp0s8 -o enp0s9 -j ACCEPT
 
 
 #dar drop a tudo
